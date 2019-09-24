d032b00aaa95097a6258c2845f6a69dce41b362a
super-lin0 <3924578651@qq.com>
2019-09-24 15:43:36 +0800

    Add css-module

---
+++ webpackinaction/05-handle-styles/css-module/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const htmlPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  mode: "development",
+  plugins: [new htmlPlugin({ title: path.basename(__dirname) })],
+  devServer: {
+    publicPath: "/dist/",
+    port: 3000
+  }
+};
